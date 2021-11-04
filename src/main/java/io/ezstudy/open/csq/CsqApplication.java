50d5ad724e827cca421f95a3fa6af2fd2876e0c3
ksj <rhtjrwnszyzy@gmail.com>
2021-11-04 21:05:38 +0900

    first init

---
+++ src/main/java/io/ezstudy/open/csq/CsqApplication.java
@@ -0,0 +1,13 @@
+package io.ezstudy.open.csq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ezstudy/open/csq/CsqApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ezstudy.open.csq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
